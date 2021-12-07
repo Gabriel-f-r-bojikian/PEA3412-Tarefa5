42fc9a37bf72c7ba537af54e8b71525a09141be3
Gabriel Fernandes Rosa Bojikian <gabriel.bojikian.dev@gmail.com>
2021-12-07 16:14:36 -0300

    Função main.m criada

---
+++ main.m
@@ -0,0 +1,26 @@
+%{
+  PEA 3412 - Tarefa 5
+  
+  Grupo G:
+    - Gabriel Fernandes Rosa Bojikian, 9349221
+    - Maurício Kenji Sanda, 10773190
+    - Pedro César Igarashi, 10812071
+
+  Descrição:
+    Esta é a função principal do script de simulação da tarefa 5. Ele automatiza 
+    o processo de aquisição e tratamento dos sinais de corrente e realiza a
+    simulação da proteção Mho utilizando a decomposição em componentes
+    simétricas das correntes.
+%}
+
+close all;
+fclose all;
+clear all;
+clc;
+
+filename = '';
+
+[ iaLocal, ibLocal, icLocal, iaRemoto, ibRemoto, icRemoto ] = adquire_sinal(filename);
+
+[zeroLocal, diretaLocal, inversaLocal] = calcula_componentes_simetricas(iaLocal, ibLocal, icLocal);
+[zeroRemoto, diretaRemoto, inversaRemoto] = calcula_componentes_simetricas(iaRemoto, ibRemoto, icRemoto);
